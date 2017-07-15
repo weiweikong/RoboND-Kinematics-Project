fix: correct theta2 calculation

kuka_arm/scripts/IK_debug.py
@@ -165,10 +165,14 @@ def test_code(test_case):
         px = req.poses[x].position.x
         py = req.poses[x].position.y
         pz = req.poses[x].position.z
+        
+        print("px = %f, py = %f, pz = %f", px, py, pz)
 
         (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
             [req.poses[x].orientation.x, req.poses[x].orientation.y,
                 req.poses[x].orientation.z, req.poses[x].orientation.w])
+
+        print("roll = %f, pitch = %f, yaw = %f", roll, pitch, yaw)
      
         # Calculate joint angles using Geometric IK method
 
@@ -181,17 +185,13 @@ def test_code(test_case):
         # b = l3 + l4 w/ adjustment
         l3_4 = 0.96 # from URDF file
         l4_5 = 0.54 # from URDF file
-        l3_4_offset = a3
+        l3_4_offset = abs(a3)
         l3_4_offset = l3_4_offset.subs(s)
-        l3_4_angle = asin(l3_4_offset / l3_4)
+        l3_4_angle = pi - asin(l3_4_offset / l3_4)
         # Cosine rule
         dist3_5 = sqrt(l3_4**2 + l4_5**2 - 2*l3_4*l4_5*cos(l3_4_angle))
+        print("dist3_5 = %f", dist3_5)
             
-        J2_x = a1
-        J2_x = J2_x.subs(s)
-        J2_z = d1
-        J2_z = J2_z.subs(s)
-
         # 1. Find total rotation matrix from roll-pitch-yaw data
         R_total = simplify(rot_x(roll) * rot_y(pitch) * rot_z(yaw))
 
@@ -213,15 +213,34 @@ def test_code(test_case):
 
         # 3. theta1 calc
         theta1 = atan2(w_c[1,0], w_c[0,0])
+        print("theta1 = ", theta1)
 
         # 4. theta2 calc
+        J2_x = a1 * cos(theta1)
+        J2_x = J2_x.subs(s)
+        J2_y = a1 * sin(theta1)
+        J2_y = J2_y.subs(s)
+        J2_z = d1 
+        J2_z = J2_z.subs(s)
+        J5_x = w_c[0,0]
+        J5_y = w_c[1,0]
+        J5_z = w_c[2,0]
+
+        dist_J2_J5 = sqrt((J5_x - J2_x)**2 + (J5_y - J2_y)**2 + (J5_z - J2_z)**2)
+        dist_J2_J5_xy = sqrt((J5_x - J2_x)**2 + (J5_y - J2_y)**2)
         xc = w_c[0,0] - J2_x
         yc = w_c[2,0] - J2_z
-        theta2 = pi/2 - (acos((dist3_5**2 - l2_3**2 - xc**2 - yc**2)/(-2*l2_3*sqrt(xc**2 + yc**2))))
+        print("xc = ", xc)
+        print("yc = ", yc)
+        acos_innards = (dist3_5**2 - l2_3**2 - dist_J2_J5**2)/(-2*l2_3*dist_J2_J5)
+        print("acos innards = ", acos_innards)
+        theta2 = pi/2 - (acos((dist3_5**2 - l2_3**2 - dist_J2_J5**2)/(-2*l2_3*dist_J2_J5))) + acos(dist_J2_J5_xy/dist_J2_J5) 
+        print("theta2 = ", theta2)
            
         # 5. theta3 calc
-        theta3 = pi/2 - atan2(sqrt(1 - ((xc**2 + yc**2 - l2_3**2 - dist3_5**2) / (-2*l2_3*dist3_5))),
-                                (xc**2 + yc**2 - l2_3**2 - dist3_5**2) / (-2*l2_3*dist3_5))
+        theta3 = pi/2 - atan2(sqrt(1 - ((dist_J2_J5**2 - l2_3**2 - dist3_5**2) / (-2*l2_3*dist3_5))),
+                                (dist_J2_J5**2 - l2_3**2 - dist3_5**2) / (-2*l2_3*dist3_5))
+        print("theta3 = ", theta3)
 
         # Choose other theta3 calc
         # theta3_b = pi/2 - atan2(-sqrt(1 - ((xc**2 + yc**2 - l2_3**2 - dist3_5**2) / (-2*l2_3*dist3_5))),